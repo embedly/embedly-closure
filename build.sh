1761c1c73c8ab051087094d6ef59252c04bcb47f
Bob Corsaro <bob@embed.ly>
2011-02-28 12:35:33 -0500

    Improve lib

--- build.sh
+++ build.sh
@@ -1,9 +1,13 @@
-python ../closure-library/closure/bin/build/closurebuilder.py \
+#!/bin/sh
+
+CLOSURE_LIB="../closure-library"
+CLOSURE_COMPILER="../closure-compiler"
+
+python "${CLOSURE_LIB}/closure/bin/build/closurebuilder.py" \
   --namespace embedly.Api \
   --root . \
-  --root ../closure-library \
+  --root "${CLOSURE_LIB}" \
   -o compiled \
-  -c ../closure-compiler/compiler.jar \
+  -c "${CLOSURE_COMPILER}/compiler.jar" \
   -f --compilation_level=ADVANCED_OPTIMIZATIONS \
-  --output_file=embedly.min.js \
-  -f --closure_entry_point=embedly.Api
+  --output_file=embedly.min.js
--- embedly.js
+++ embedly.js
@@ -3,40 +3,82 @@ goog.provide('embedly.Api')
 goog.require('goog.net.Jsonp')
 
 /**
+ * Creates a new Embedly api object that can be used to call Embedly
+ * endpoints.
+ *
+ * @param {Object=} args An object with constructor params.  Accepted
+ *     params are::
+ *       key:     The Embedly Pro api key.  If this is set then we will use
+ *                http://pro.embed.ly as the default host.
+ *
+ *       host:    The Embedly api host.  This is http://pro.embed.ly by 
+ *                default if 'key' is set, or http://api.embed.ly otherwise
+ *
+ *       timeout: Timeout in milis on calls. Default is 120000 (120 seconds)
+ *
  * @constructor
  */
 embedly.Api = function(args) {
-  this.host = args['host'] || 'http://pro.embed.ly'
+  if (!args) args = {}
+  if (args['key']) {
+    this.key = args['key']
+    this.host = args['host'] || 'http://pro.embed.ly'
+  } else {
+    this.host = args['host'] || 'http://api.embed.ly'
+  }
   this.paths = {
       'oembed': '/1/oembed'
     , 'objectify': '/2/objectify'
     , 'preview': '/1/preview'
   }
-  this.key = args['key']
+  this.timeout = args['timeout'] || 120000
 }
 
-embedly.Api.prototype.call = function(args) {
-  var path = this.paths[args['endpoint']]
-  //var encoded_urls = []
-  //for (i in args['urls']) {
-   // encoded_urls.push(encodeURIComponent(args['urls'][i]))
-  //}
+/**
+ * Call an Embedly endpoint.  The reply is passed to resultCallback.  If no
+ * result is recieved before the timeout, then the original url parameters
+ * are passed back to the opt_errorCallback.
+ *
+ * @param {string=} endpoint Either 'oembed', 'objectify' or 'preview'.  For
+ *     non-pro accounts, this should always be 'oembed'.
+ *
+ * @param {Object=} params Name value pairs to send as query params to the
+ *     endpoint.  If 'key' was set in the constructor, then it will
+ *     automatically be add to the params.  'urls' is required and should
+ *     be an array of at least one url.
+ *
+ * @param {Function=} resultCallback Callback function expecting one argument 
+ *     that is passed resulting object returned from the endpoint.  The result
+ *     is an array of Objects in that match up to the params.urls in the same
+ *     order that they were given.
+ *
+ * @param {Function=} opt_errorCallback Callback function expecting one
+ *     argument.  Usually this is called on a timeout error.
+ */
+embedly.Api.prototype.call = function(endpoint, params, resultCallback, opt_errorCallback) {
+  var path = this.paths[endpoint]
   var jsonp = new goog.net.Jsonp(this.host+path)
-  var payload = {
-      'key': this.key
-    , 'urls': args['urls']
+  jsonp.setRequestTimeout(this.timeout)
+  if (!params['key'] && this['key']) {
+    params['key'] = this.key
   }
   jsonp.send(
-      payload
-    , function() {
-        args.complete(arguments)
+      params
+    , function(objs) {
+        resultCallback(objs)
       }
-    , function() {
-        args.error(arguments)
+    , function(payload) {
+        if (opt_errorCallback) {
+          opt_errorCallback(payload)
+        }
       }
   )
 }
 
+/**
+ * The following is only needed if you are using this library outside of
+ * closure-library as a general purpose lib.
+ */
 goog.exportSymbol('embedly.Api', embedly.Api)
 goog.exportProperty(embedly.Api.prototype, 'call', embedly.Api.prototype.call)
 
